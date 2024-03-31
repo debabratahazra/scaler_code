aa97bd5ade066ca065594e9c3300722e7a953d1c
Hazra <debabrata.hazra@infineon.com>
2024-03-31 11:01:52 +0530

    Added hypothesis testing revision sheet

--- scaler_adv_py/netflix-case-study/case-study.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df = pd.read_csv("./scaler_adv_py/netflix-case-study/netflix.csv")
-print(df.columns)
